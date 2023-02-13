7c568488054c5fe14ee6f1de750e48708f7717f7
whfh3900 <whfh3900@naver.com>
2023-02-13 15:05:01 +0900

    2023-02-13 update

--- real/settings.py
+++ real/settings.py
@@ -79,13 +79,13 @@ WSGI_APPLICATION = 'real.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = local_settings.DATABASES
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+DATABASES = local_settings.DATABASES
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 
 
