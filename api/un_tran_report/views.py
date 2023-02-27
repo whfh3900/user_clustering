3243346a289fcc0711da979b98bae4f329f07253
whfh3900 <whfh3900@naver.com>
2023-02-27 13:06:53 +0900

    2023-02-27 수정

--- api/un_tran_report/views.py
+++ api/un_tran_report/views.py
@@ -37,63 +37,67 @@ class UserInfoViewAPI(APIView):
 
         except Exception as e:
             # url이 잘못 됬을때(user_info 에 없는 UID)
-            return Response({"data": None, "state": 404, "error": str(e)}, status=status.HTTP_404_NOT_FOUND)
+            # return Response({"data": None, "state": 404, "error": str(e)}, status=status.HTTP_404_NOT_FOUND)
+            return Response({"data": None, "state": 404, "error": "없는 UID입니다. UID를 확인해주세요."}, status=status.HTTP_404_NOT_FOUND)
 
 
     def post(self, request, pk=None):
-        serializer = TransactionSerializer(data=request.data)
+        try:
+            serializer = TransactionSerializer(data=request.data)
+        except Exception as e:
+            # serializer 형식 및 url이 잘못 됬을때(user_info 에 없는 UID)
+            # return Response({"result": None, "state": 404, "error": serializer.errors}, status=status.HTTP_404_NOT_FOUND)
+            return Response({"result": None, "state": 404, "error": "없는 UID입니다. UID를 확인해주세요."}, status=status.HTTP_404_NOT_FOUND)
+
         if serializer.is_valid():
             # serializer.save()
 
-            try:
-                # 해당 User 데이터 가져오기
-                tran_use = UserInfo.objects.filter(uid=pk).values()[0]
+            # 해당 User 데이터 가져오기
+            tran_use = UserInfo.objects.filter(uid=pk).values()[0]
 
-                # 거래유형
-                result = utd.predict_result(serializer.data)
+            # 거래유형
+            result = utd.predict_result(serializer.data)
 
-                # 특이거래 여부
-                if tran_use[result] != 0:
-                    detection_result = False
-                else:
-                    detection_result = True
+            # 특이거래 여부
+            if tran_use[result] != 0:
+                detection_result = False
+            else:
+                detection_result = True
 
-                # UserInfo 업데이트
-                tran_use[result] = tran_use[result]+1
-                get_uid = UserInfo.objects.get(uid=pk)
+            # UserInfo 업데이트
+            tran_use[result] = tran_use[result]+1
+            get_uid = UserInfo.objects.get(uid=pk)
 
-                get_uid.c1 = tran_use["c1"]
-                get_uid.c2 = tran_use["c2"]
-                get_uid.c3 = tran_use["c3"]
-                get_uid.c4 = tran_use["c4"]
-                get_uid.c5 = tran_use["c5"]
-                get_uid.c6 = tran_use["c6"]
-                get_uid.c7 = tran_use["c7"]
-                get_uid.c8 = tran_use["c8"]
-                get_uid.c9 = tran_use["c9"]
-                get_uid.c10 = tran_use["c10"]
-                get_uid.c11 = tran_use["c11"]
-                get_uid.c12 = tran_use["c12"]
-                get_uid.save()
+            get_uid.c1 = tran_use["c1"]
+            get_uid.c2 = tran_use["c2"]
+            get_uid.c3 = tran_use["c3"]
+            get_uid.c4 = tran_use["c4"]
+            get_uid.c5 = tran_use["c5"]
+            get_uid.c6 = tran_use["c6"]
+            get_uid.c7 = tran_use["c7"]
+            get_uid.c8 = tran_use["c8"]
+            get_uid.c9 = tran_use["c9"]
+            get_uid.c10 = tran_use["c10"]
+            get_uid.c11 = tran_use["c11"]
+            get_uid.c12 = tran_use["c12"]
+            get_uid.save()
 
-                # Transaction 추가
-                Transaction.objects.create(bas_ym=serializer.data['bas_ym'], \
-                                           age_dc=serializer.data['age_dc'], \
-                                           gender=serializer.data['gender'], \
-                                           bas_dt=serializer.data['bas_dt'], \
-                                           tran_md=serializer.data['tran_md'], \
-                                           ats_kdcd_dtl=serializer.data['ats_kdcd_dtl'], \
-                                           dps_trn_am=serializer.data['dps_trn_am'], \
-                                           text_1=serializer.data['text_1'], \
-                                           user_info_uid=get_uid, \
-                                           result=result)
+            # Transaction 추가
+            Transaction.objects.create(bas_ym=serializer.data['bas_ym'], \
+                                       age_dc=serializer.data['age_dc'], \
+                                       gender=serializer.data['gender'], \
+                                       bas_dt=serializer.data['bas_dt'], \
+                                       tran_md=serializer.data['tran_md'], \
+                                       ats_kdcd_dtl=serializer.data['ats_kdcd_dtl'], \
+                                       dps_trn_am=serializer.data['dps_trn_am'], \
+                                       text_1=serializer.data['text_1'], \
+                                       user_info_uid=get_uid, \
+                                       result=result)
 
-                # 정상 작동
-                return Response({"result": detection_result, "state": 201, "error": None}, status=status.HTTP_201_CREATED)
-            except Exception as e:
-                # 값의 형식이 잘못 됬을때(ex. tran_md -> 출금 X, 지급 O)
-                return Response({"result": None, "state": 400, "error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
+            # 정상 작동
+            return Response({"result": detection_result, "state": 201, "error": None}, status=status.HTTP_201_CREATED)
+
 
         else:
-            # serializer 형식 및 url이 잘못 됬을때(user_info 에 없는 UID)
-            return Response({"result": None, "error": serializer.errors, "state": 404}, status=status.HTTP_404_NOT_FOUND)
+            # 값의 형식이 잘못 됬을때(ex. tran_md -> 출금 X, 지급 O)
+            return Response({"result": None, "state": 400, "error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
